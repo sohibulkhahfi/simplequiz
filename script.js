b7f3e3af864ef7a783e7f54f2d22e3b5d261abeb
Sohibul Khahfi <sohibulkhahfi140310@gmail.com>
2023-07-21 02:44:00 +0700

    Membuat kuis haengeul 10 kata korea yang sering digunakan

--- script.js
+++ script.js
@@ -1,41 +1,220 @@
-const questions = [
+const kosakataKorea = [
   {
-    question: "Which is largest animal in the world?",
-    answers: [
-      { text: "Shark", correct: false },
-      { text: "Blue Whale", correct: true },
-      { text: "Elephant", correct: false },
-      { text: "Giraffe", correct: false },
-    ],
+    hangeul: "안녕하세요",
+    pelafalan: "annyeonghaseyo",
+    arti: "Halo",
+    contohKalimat: "안녕하세요! 저는 한국에서 왔어요.",
+    pelafalanKalimat: "annyeonghaseyo! Jeoneun Hangukeseo wasseoyo.",
+    artiKalimat: "Halo! Saya berasal dari Korea.",
   },
   {
-    question: "Which is the smallest country in the world?",
-    answers: [
-      { text: "Vatican City", correct: true },
-      { text: "Bhutan", correct: false },
-      { text: "Nepal", correct: false },
-      { text: "Shri Lanka", correct: false },
-    ],
+    hangeul: "감사합니다",
+    pelafalan: "gamsahamnida",
+    arti: "Terima kasih",
+    contohKalimat: "감사합니다, 친절하시네요.",
+    pelafalanKalimat: "gamsahamnida, chinjeolhasineyo.",
+    artiKalimat: "Terima kasih, Anda sangat ramah.",
   },
   {
-    question: "Which is largest desert in the world?",
-    answers: [
-      { text: "Kalahari", correct: false },
-      { text: "Gobi", correct: false },
-      { text: "Sahara", correct: false },
-      { text: "Antartica", correct: true },
-    ],
+    hangeul: "사랑해요",
+    pelafalan: "saranghaeyo",
+    arti: "Aku cinta padamu",
+    contohKalimat: "엄마, 사랑해요!",
+    pelafalanKalimat: "eomma, saranghaeyo!",
+    artiKalimat: "Ibu, aku mencintaimu!",
   },
   {
-    question: "Which is the smallest continent in the world?",
-    answers: [
-      { text: "Asia", correct: false },
-      { text: "Australia", correct: true },
-      { text: "Arctic", correct: false },
-      { text: "Africa", correct: false },
-    ],
+    hangeul: "미안해요",
+    pelafalan: "mianhaeyo",
+    arti: "Maaf",
+    contohKalimat: "미안해요, 제가 실수했어요.",
+    pelafalanKalimat: "mianhaeyo, jega silsuhaesseoyo.",
+    artiKalimat: "Maaf, saya membuat kesalahan.",
+  },
+  {
+    hangeul: "네",
+    pelafalan: "ne",
+    arti: "Ya",
+    contohKalimat: "네, 알겠습니다.",
+    pelafalanKalimat: "ne, algesseumnida.",
+    artiKalimat: "Ya, saya mengerti.",
+  },
+  {
+    hangeul: "아니요",
+    pelafalan: "aniyo",
+    arti: "Tidak",
+    contohKalimat: "아니요, 그것은 틀렸어요.",
+    pelafalanKalimat: "aniyo, geugeoseun teullyeosseoyo.",
+    artiKalimat: "Tidak, itu salah.",
+  },
+  {
+    hangeul: "어떻게요",
+    pelafalan: "eotteokeyo",
+    arti: "Bagaimana?",
+    contohKalimat: "오늘 저녁에 뭐 해요? 어떻게요?",
+    pelafalanKalimat: "oneul jeonyeoge mwo haeyo? eotteokeyo?",
+    artiKalimat: "Apa yang akan kita lakukan malam ini? Bagaimana?",
+  },
+  {
+    hangeul: "뭐",
+    pelafalan: "mwo",
+    arti: "Apa?",
+    contohKalimat: "뭐? 이해하지 못해요.",
+    pelafalanKalimat: "mwo? ihaehaji mothaeyo.",
+    artiKalimat: "Apa? Saya tidak mengerti.",
+  },
+  {
+    hangeul: "이해해요",
+    pelafalan: "ihaehaeyo",
+    arti: "Saya mengerti",
+    contohKalimat: "네 말을 이해해요.",
+    pelafalanKalimat: "ne mareul ihaehaeyo.",
+    artiKalimat: "Saya mengerti apa yang kamu katakan.",
+  },
+  {
+    hangeul: "좋아요",
+    pelafalan: "joayo",
+    arti: "Bagus / Suka",
+    contohKalimat: "이 음식 정말 맛있어요. 많이 좋아요.",
+    pelafalanKalimat: "i eumsik jeongmal masisseoyo. manhi joayo.",
+    artiKalimat: "Makanan ini sangat lezat. Saya sangat menyukainya.",
+  },
+  {
+    hangeul: "싫어요",
+    pelafalan: "sireoyo",
+    arti: "Tidak suka",
+    contohKalimat: "저는 고양이를 좋아해요, 하지만 개는 싫어요.",
+    pelafalanKalimat: "jeoneun goyangireul joahaeyo, hajiman gaeneun sireoyo.",
+    artiKalimat: "Saya suka kucing, tapi saya tidak suka anjing.",
+  },
+  {
+    hangeul: "도와주세요",
+    pelafalan: "dowajuseyo",
+    arti: "Tolong bantu saya",
+    contohKalimat: "제가 방금 지갑을 잃어버렸어요. 도와주세요!",
+    pelafalanKalimat: "jega banggeum jigaubeoryeosseoyo. dowajuseyo!",
+    artiKalimat: "Saya baru saja kehilangan dompet saya. Tolong bantu saya!",
+  },
+  {
+    hangeul: "물",
+    pelafalan: "mul",
+    arti: "Air",
+    contohKalimat: "목이 마르네요. 물 주세요.",
+    pelafalanKalimat: "mogi mareuneyo. mul juseyo.",
+    artiKalimat: "Saya haus. Tolong berikan air.",
+  },
+  {
+    hangeul: "음식",
+    pelafalan: "eumsik",
+    arti: "Makanan",
+    contohKalimat: "한국 음식은 매우 맛있어요.",
+    pelafalanKalimat: "Hanguk eumsikeun maeu masisseoyo.",
+    artiKalimat: "Makanan Korea sangat lezat.",
+  },
+  {
+    hangeul: "학교",
+    pelafalan: "hakgyo",
+    arti: "Sekolah",
+    contohKalimat: "저는 학교에 갑니다.",
+    pelafalanKalimat: "jeoneun hakgyoe gamnida.",
+    artiKalimat: "Saya pergi ke sekolah.",
+  },
+  {
+    hangeul: "집",
+    pelafalan: "jip",
+    arti: "Rumah",
+    contohKalimat: "오늘은 집에서 쉬겠어요.",
+    pelafalanKalimat: "oneureun jibeseo swigesseoyo.",
+    artiKalimat: "Hari ini saya akan istirahat di rumah.",
+  },
+  {
+    hangeul: "친구",
+    pelafalan: "chingu",
+    arti: "Teman",
+    contohKalimat: "저의 친구들과 함께 파티에 가요.",
+    pelafalanKalimat: "jeoui chingudeulgwa hamkke patie gayo.",
+    artiKalimat: "Saya akan pergi ke pesta bersama teman-teman saya.",
+  },
+  {
+    hangeul: "가방",
+    pelafalan: "gabang",
+    arti: "Tas",
+    contohKalimat: "가방을 잊어버렸어요. 집에 다시 가져와야 해요.",
+    pelafalanKalimat: "gabangeul ijeobeoryeosseoyo. jibe dasi gajyeowaya haeyo.",
+    artiKalimat: "Saya lupa tas saya. Saya harus mengambilnya kembali di rumah.",
+  },
+  {
+    hangeul: "기차",
+    pelafalan: "gicha",
+    arti: "Kereta api",
+    contohKalimat: "서울로 가는 기차가 몇 시에 있어요?",
+    pelafalanKalimat: "Seoulloro ganeun gichaga myeot sie isseoyo?",
+    artiKalimat: "Pukul berapa kereta menuju Seoul?",
+  },
+  {
+    hangeul: "버스",
+    pelafalan: "beoseu",
+    arti: "Bus",
+    contohKalimat: "이 버스는 시내를 다니나요?",
+    pelafalanKalimat: "i beoseuneun sinaereul daninayo?",
+    artiKalimat: "Apakah bus ini beroperasi di kota?",
   },
 ];
+
+const questions = [];
+
+// Ambil 10 data pertama dari kosakataKorea untuk membuat 10 soal
+const randomIndices = [];
+while (randomIndices.length < 10) {
+  const randomIndex = Math.floor(Math.random() * kosakataKorea.length);
+  if (!randomIndices.includes(randomIndex)) {
+    randomIndices.push(randomIndex);
+  }
+}
+
+for (let i = 0; i < 10; i++) {
+  const kosakataIndex = randomIndices[i];
+  const currentKosakata = kosakataKorea[kosakataIndex];
+
+  const question = {
+    hangeul: currentKosakata.hangeul,
+    pelafalan: currentKosakata.pelafalan,
+    arti: currentKosakata.arti,
+    contohKalimat: currentKosakata.contohKalimat,
+    question: "Kata manakah yang berarti " + currentKosakata.arti + "?",
+    answers: [
+      {
+        text: kosakataKorea[kosakataIndex].hangeul,
+        correct: true,
+        arti: kosakataKorea[kosakataIndex].arti,
+        pelafalan: kosakataKorea[kosakataIndex].pelafalan,
+      },
+    ],
+  };
+
+  // Ambil 3 kata lain secara acak untuk dijadikan pilihan jawaban
+  const randomAnswerIndices = [];
+  while (randomAnswerIndices.length < 3) {
+    const randomAnswerIndex = Math.floor(Math.random() * kosakataKorea.length);
+    if (!randomAnswerIndices.includes(randomAnswerIndex) && randomAnswerIndex !== kosakataIndex) {
+      randomAnswerIndices.push(randomAnswerIndex);
+    }
+  }
+
+  for (const index of randomAnswerIndices) {
+    question.answers.push({
+      text: kosakataKorea[index].hangeul,
+      correct: false,
+      arti: kosakataKorea[index].arti,
+      pelafalan: kosakataKorea[index].pelafalan,
+    });
+  }
+
+  // Acak urutan pilihan jawaban
+  question.answers.sort(() => Math.random() - 0.5);
+
+  questions.push(question);
+}
 
 const quiestionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
@@ -59,7 +238,7 @@ function showQuestion() {
 
   currentQuestion.answers.forEach((answer) => {
     const button = document.createElement("button");
-    button.innerHTML = answer.text;
+    button.innerHTML = `${answer.text} (${answer.pelafalan}) <strong class="hidden">= ${answer.arti}</strong>`;
     button.classList.add("btn");
     answerButtons.appendChild(button);
     if (answer.correct) {
@@ -74,6 +253,13 @@ function resetState() {
   while (answerButtons.firstChild) {
     answerButtons.removeChild(answerButtons.firstChild);
   }
+}
+
+function showMeaning() {
+  const meanings = answerButtons.querySelectorAll("strong");
+  meanings.forEach((meaning) => {
+    meaning.classList.remove("hidden");
+  });
 }
 
 function selectAnswer(e) {
@@ -91,6 +277,7 @@ function selectAnswer(e) {
     }
     button.disabled = true;
   });
+  showMeaning();
   nextButton.style.display = "block";
 }
 
